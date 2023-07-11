2aa1f25d452e5f96c80f44925991e549da5cb6ae
dududornelees <eduardo.dsdorneles@gmail.com>
2023-07-11 15:32:26 -0300

    feature: classe para carros

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Car firstCar = new Car("BMW", "X6", 2023, 125000);
+        System.out.println("O veículo é uma: " + firstCar.getBrand() + " " + firstCar.getModel() + " " + firstCar.getYear() + " e o preço é: R$" + firstCar.getPrice());
     }
 }
